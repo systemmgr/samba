c4c4f03d38d6f6f6d3fee294d46fb5b520f02923
casjay <git-admin@casjaysdev.com>
2023-08-10 20:31:25 -0400

    🗃️ Updated domain names 🗃️
    install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version           :  202207031529-git
 # @@Author           :  Jason Hempstead
-# @@Contact          :  jason@casjaysdev.com
+# @@Contact          :  jason@casjaysdev.pro
 # @@License          :  LICENSE.md
 # @@ReadME           :  install.sh --help
 # @@Copyright        :  Copyright: (c) 2023 Jason Hempstead, Casjays Developments
